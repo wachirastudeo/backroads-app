d19c7ef9a86f485f6d179e9bf6eed9178eae5349
wachirastudeo <wachirastude@gmail.com>
2026-01-07 12:58:34 +0700

    create backroad app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+const  Navbar  =  ()  =>  {
+    return  (
+      <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src="../public/images/logo.svg" className="nav-logo" alt="backroads" />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>    
+        <ul className="nav-links" id="nav-links">
+          <li>
+            <a href="#home" className="nav-link"> home </a>
+          </li>
+
+          <li>
+            <a href="#about" className="nav-link"> about </a>
+          </li>
+
+          <li>
+            <a href="#services" className="nav-link"> services </a>
+          </li>
+
+          <li>
+            <a href="#tours" className="nav-link"> tours</a>
+          </li>
+        </ul>
+
+        <ul className="nav-icons">
+          <li>
+
+            <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" className="nav-icon">
+            
+            <i className="fab fa-facebook" /></a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" className="nav-icon"
+              ><i className="fab fa-twitter"></i
+            ></a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer"className="nav-icon"
+              ><i className="fab fa-squarespace"></i
+            ></a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    );
+  };
+  
+  export default Navbar;
+  
+
