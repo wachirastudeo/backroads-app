c0bb21c6b2555bb5d24674e7d83d5102637e91a8
wachirastudeo <wachirastude@gmail.com>
2026-01-07 19:52:11 +0700

    Remove old logo.svg and update Navbar to use new logo from images directory; adjust global styles in index.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
+import logo from '../images/logo.svg';
 const  Navbar  =  ()  =>  {
     return  (
       <nav className="navbar">
       <div className="nav-center">
         <div className="nav-header">
-          <img src="../public/images/logo.svg" className="nav-logo" alt="backroads" />
+          <img src={logo} id="logo" className="nav-logo" alt="backroads" />
           <button type="button" className="nav-toggle" id="nav-toggle">
             <i className="fas fa-bars"></i>
           </button>
